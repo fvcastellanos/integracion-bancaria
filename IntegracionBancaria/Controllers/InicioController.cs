82b2747b19ad1142cf15136466b4ba2f51636838
Francisco Castellanos <fvcastellanos@gmail.com>
2017-05-22 22:46:58 -0600

    21: Inicio de sesion, plantilla de sesion, salida

--- IntegracionBancaria/Controllers/InicioController.cs
+++ IntegracionBancaria/Controllers/InicioController.cs
@@ -2,11 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IntegracionBancaria.Controllers
 {
-    public class InicioController : Controller
+    public class InicioController : SesionController
     {
         public IActionResult Index()
         {
-            return View();
+            var perfil = ObtenerUsuario();
+            
+            return View(perfil);
+        }
+
+        public IActionResult Salir()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/");
         }
     }
 }
