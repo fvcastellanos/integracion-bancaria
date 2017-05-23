e7ece5a32b17b50b400e7638c69cd47d0ed79a0d
Jorge Alvarez <alvarez.jeap@gmail.com>
2017-05-22 22:51:13 -0600

    22: Se removio codigo no necesario

--- IntegracionBancaria/Controllers/MockController.cs
+++ IntegracionBancaria/Controllers/MockController.cs
@@ -15,20 +15,11 @@ namespace IntegracionBancaria.Controllers
         [HttpGet("Usuarios/{usuarioId}/Bancos/{bancoId}/Cuentas")]
         public List<Cuenta> GetCuentas(long usuarioId, long bancoId)
         {
-        
-        // using (StreamReader r = new StreamReader("Resources/usuarios.json"))
-        // {
-        //     string json = r.ReadToEnd();
-        //     List<Usuario> items = JsonConvert.DeserializeObject<List<Usuario>>(json);
-        // }
-            // JsonConvert.DeserializeObject(json);
-            
             var usuarios = JsonConvert.DeserializeObject<List<Usuario>>(System.IO.File.ReadAllText("./Resources/usuarios.json"));
             var usuario = usuarios.Find(u => u.Id == usuarioId);
             var bancos = usuario.Bancos.Find(b => b.Id == bancoId);
-            var cuentas = bancos.Cuentas;
             
-            return cuentas;
+            return bancos.Cuentas;
         }
 
     }
